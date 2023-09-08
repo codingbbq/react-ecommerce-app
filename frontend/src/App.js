b723e0dd33f83d5ec9dec90f1be4976500309dd6
codingbbq <codingbbq@gmail.com>
2023-09-09 00:00:23 +0530

    Header, footer and product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
-    <div>React Ecommerce App</div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
