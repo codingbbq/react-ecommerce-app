814f2644e913e13e7e8146a651f1471af5a2c9b0
codingbbq <codingbbq@gmail.com>
2023-09-14 22:20:26 +0530

    Manage state for adding items to Cart

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,10 +1,13 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap';
 import Rating from '../components/Rating';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../slices/cartSlice';
 
 const ProductScreen = () => {
     const { id: productId } = useParams();
@@ -14,6 +17,18 @@ const ProductScreen = () => {
         error,
     } = useGetProductDetailsQuery(productId);
 
+    const [qty, setQty] = useState(0);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const addToCartHandler = () => {
+        dispatch(addToCart({
+            ...product, qty
+        }));
+        navigate('/cart');
+    };
+
     return (
         <>
             <Link className='btn btn-light my-3' to='/'>
@@ -22,7 +37,9 @@ const ProductScreen = () => {
             {isLoading ? (
                 <Loader />
             ) : error ? (
-                <Message variant='danger'>{error?.data?.message || error?.error}</Message>
+                <Message variant='danger'>
+                    {error?.data?.message || error?.error}
+                </Message>
             ) : (
                 <>
                     <Row>
@@ -77,6 +94,26 @@ const ProductScreen = () => {
                                             </Col>
                                         </Row>
                                     </ListGroup.Item>
+
+                                    {product.countInStock > 0 && (
+                                        <ListGroup.Item>
+                                            <Row>
+                                                <Col>Qty: </Col>
+                                                <Col>
+                                                    <Form.Control
+                                                        as='select'
+                                                        value={qty}
+                                                        onChange={(e) => setQty(Number(e.target.value))}
+                                                    >
+                                                        {[...Array(product.countInStock).keys()].map((x) => (
+                                                            <option value={x+1} key={x+1}>{x+1}</option>
+                                                        ))}
+                                                    </Form.Control>
+                                                </Col>
+                                            </Row>
+                                        </ListGroup.Item>
+                                    )}
+
                                     <ListGroup.Item>
                                         <Button
                                             className='btn-block'
@@ -84,6 +121,7 @@ const ProductScreen = () => {
                                             disabled={
                                                 product.countInStock === 0
                                             }
+                                            onClick={addToCartHandler}
                                         >
                                             Add to Cart
                                         </Button>
