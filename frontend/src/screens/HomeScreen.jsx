a4545db769baf792e07ce725c7d2b7ca76e53fc1
codingbbq <codingbbq@gmail.com>
2023-09-14 15:17:07 +0530

    Implement Redux ToolKit for store and fetch data

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,34 +1,32 @@
-import React from 'react'
+import React from 'react';
 import { Row, Col } from 'react-bootstrap';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 import Product from '../components/Product';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomeScreen = () => {
+    const { data: products, isLoading, error } = useGetProductsQuery();
+    return (
+        <>
+            {isLoading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant='danger'>{error?.data?.message || error?.error}</Message>
+            ) : (
+                <>
+                    <h1>Latest products</h1>
+                    <Row>
+                        {products.map((product) => (
+                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                <Product product={product}></Product>
+                            </Col>
+                        ))}
+                    </Row>
+                </>
+            )}
+        </>
+    );
+};
 
-  const [products, setProducts] = useState([]);
-  const fetchProduct = async () => {
-    const {data} = await axios.get('/api/products');
-    setProducts(data);
-  }
-  useEffect(() => {
-    fetchProduct();
-  }, [])
-
-  return (
-    <>
-        <h1>Latest products</h1>
-        <Row>
-        {
-            products.map((product) => (
-                <Col sm={12} md={6} lg={4} xl={3}>
-                    <Product product={product}></Product>
-                </Col>
-            ))
-        }            
-        </Row>
-    </>
-  )
-}
-
-export default HomeScreen
+export default HomeScreen;
