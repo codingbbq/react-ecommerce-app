c961cab5da576e1a14dd37d8548f23fde7ca63a8
codingbbq <codingbbq@gmail.com>
2023-09-16 22:08:39 +0530

    Login screen

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -15,7 +15,7 @@ import { protect, admin } from '../middleware/authHandler.js';
 const router = express.Router();
 
 router.route('/').get(protect, admin, getUsers).post(registerUser);
-router.post('/login', authUser);
+router.post('/auth', authUser);
 router.post('/logout', logoutUser);
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
 router.route('/:id').get(protect, admin, getUserById).put(protect, admin, updateUser).delete(protect, admin, deleteUser);
