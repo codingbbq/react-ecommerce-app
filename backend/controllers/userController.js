6649e6f25158d08bdaa8ee8cd3034a38071b94b4
codingbbq <codingbbq@gmail.com>
2023-09-16 18:46:44 +0530

    Get profile and Update profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,11 +11,9 @@ const authUser = asyncHandler(async (req, res) => {
     const user = User.findOne({ email });
 
     if(user && (await user.matchPassword(password))) {
-
         // JWT Code
-        generateToken(res, user._id);
-        
-        res.send({
+        generateToken(res, user._id);     
+        res.status(200).json({
             _id: user._id,
             name: user.name,
             email: user.email,
@@ -48,7 +46,7 @@ const registerUser = asyncHandler(async (req, res) => {
     if(user) {
         // JWT Code
         generateToken(res, user._id);
-        
+
         res.status(201).json({
             _id: user._id,
             name: user.name,
@@ -79,7 +77,19 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @route - GET /api/users/profile
 // @access - Private
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.send('Profile  user');
+    const user = await User.findById(req.user._id);
+
+    if(user) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 
@@ -87,7 +97,28 @@ const getUserProfile = asyncHandler(async (req, res) => {
 // @route - PUT /api/users/profile
 // @access - Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.send('Profile update');
+    const user = await User.findById(req.user._id);
+
+    if(user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if(req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await User.save();
+
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
 });
 
 // @desc - Get Users
