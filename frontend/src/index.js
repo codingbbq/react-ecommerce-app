fac0b8fe78acc551337182c0ae809671baadb153
codingbbq <codingbbq@gmail.com>
2023-09-18 20:46:31 +0530

    Payment Screne

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,6 +18,7 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -29,6 +30,7 @@ const router = createBrowserRouter(
       <Route path='/register' element={ <RegisterScreen /> }></Route>
       <Route path='' element={ <PrivateRoute />}>
         <Route path='/shipping' element={ <ShippingScreen />}></Route>
+        <Route path='/payment' element={ <PaymentScreen />}></Route>
       </Route>
     </Route>
   )
