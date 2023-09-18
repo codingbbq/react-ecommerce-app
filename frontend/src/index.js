54fd7a0e8eafc2dee795720a0dabc37c409b5d2b
codingbbq <codingbbq@gmail.com>
2023-09-18 15:02:20 +0530

    Protecting shipping route with a private route component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,11 +11,13 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -25,6 +27,9 @@ const router = createBrowserRouter(
       <Route path='/cart' element={<CartScreen />}></Route>
       <Route path='/login' element={ <LoginScreen />}></Route>
       <Route path='/register' element={ <RegisterScreen /> }></Route>
+      <Route path='' element={ <PrivateRoute />}>
+        <Route path='/shipping' element={ <ShippingScreen />}></Route>
+      </Route>
     </Route>
   )
 );
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { updateCart } from '../utils/cartUtils';
 
-const initialState = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : { cartItems: [] };
+const initialState = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : { cartItems: [], shippingAddress: {}, paymentMethod: 'Paypal' };
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -10,23 +10,27 @@ const cartSlice = createSlice({
         addToCart: (state, action) => {
             const item = action.payload;
             const existsItem = state.cartItems.find((x) => x._id === item._id);
-            if(existsItem) {
-                state.cartItems = state.cartItems.map((x) => 
+            if (existsItem) {
+                state.cartItems = state.cartItems.map((x) =>
                     x._id === existsItem._id ? item : x
                 );
             } else {
                 state.cartItems = [...state.cartItems, item];
             }
-                
+
             return updateCart(state);
         },
         removeFromCart: (state, action) => {
-            state.cartItems = state.cartItems.filter((x) => x._id !== action.payload)
+            state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
+            return updateCart(state);
+        },
+        saveShippingAddress: (state, action) => {
+            state.shippingAddress = action.payload;
             return updateCart(state);
         }
     }
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress } = cartSlice.actions;
 
 export default cartSlice.reducer;
