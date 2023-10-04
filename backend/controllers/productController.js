1d5415d749b203f3769c996e11e84d759c097620
codingbbq <codingbbq@gmail.com>
2023-10-04 19:41:05 +0530

    Product update feature

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -13,8 +13,9 @@ const getProducts = asyncHandler(async (req, res) => {
 // @route - GET /api/product/:id
 // @access - Public
 const getProductById = asyncHandler(async (req, res) => {
+    console.log(req.params);
     const product = await Product.findById(req.params.id);
-    if(product) {
+    if (product) {
         res.json(product);
     } else {
         res.status(404);
@@ -22,4 +23,47 @@ const getProductById = asyncHandler(async (req, res) => {
     }
 });
 
-export { getProducts, getProductById };
+// @desc - Create a Product
+// @route - POST /api/products
+// @access - Private / Admin
+const createProduct = asyncHandler(async (req, res) => {
+    const product = new Product({
+        name: 'Sample product',
+        price: 0,
+        user: req.user._id,
+        image: '/images/sample.jpg',
+        brand: 'Sample brand',
+        category: 'Sample category',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'Sample description'
+    });
+    const createdProduct = await product.save();
+    res.status(200).json(createdProduct);
+});
+
+// @desc - Update a product
+// @route - PUT /api/products/:id
+// @access - Private / Admin
+const updateProduct = asyncHandler(async (req, res) => {
+    const { name, price, description, image, brand, category, countInStock } = req.body;
+    const product = await Product.findById(req.params.id);
+
+    if(product) {
+        product.name = name;
+        product.price = price;
+        product.description = description;
+        product.image = image;
+        product.brand = brand;
+        product.category = category;
+        product.countInStock = countInStock;
+
+        const updatedProduct = await product.save();
+        res.json(updatedProduct);
+    } else {
+        res.status(404);
+        throw new Error('Product not found');
+    }
+});
+
+export { getProducts, getProductById, createProduct, updateProduct };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getProducts, getProductById } from '../controllers/productController.js';
-
+import { getProducts, getProductById, createProduct, updateProduct } from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authHandler.js';
 const router = express.Router();
 
 router.route('/').get(getProducts);
+router.route('/').post(protect, admin, createProduct);
 router.route('/:id').get(getProductById);
+router.route('/:id').put(protect, admin, updateProduct);
 
 export default router;
