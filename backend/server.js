479b0d3844123b647975e853bf21e223d7501d8d
codingbbq <codingbbq@gmail.com>
2023-10-07 16:07:24 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,6 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middlerware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('API is running');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
